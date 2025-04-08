939e43132bc4c2900c07670c00bbc613ff366743
Tony Blum <tonyblum@Tonys-Laptop.local>
2025-04-08 12:52:32 -0600

    mgz_hd cleanupc

--- utils/replay_parser.py
+++ utils/replay_parser.py
@@ -3,7 +3,9 @@ import io
 import json
 import logging
 import hashlib
-from mgz_hd import header, summary
+from mgz_hd import header
+from mgz import summary  
+
 from utils.extract_datetime import extract_datetime_from_filename
 
 def parse_replay_full(replay_path):
