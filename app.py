dbde160cdec6feaab84a41bde4d3488710cb4d93
Tony Blum <tonyblum@Tonys-Laptop.local>
2025-04-08 18:36:46 -0600

    Add CORS support for Vercel frontendb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_migrate import Migrate, upgrade
 from db import db, init_db
